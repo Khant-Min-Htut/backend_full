3a7cd73c3fd0515a32938266d86dabe0190f6b9d
khantMinHtut <khantminhtut009@gmail.com>
2025-02-28 21:20:02 +0630

    Third commit

--- chapter_3/src/db.js
+++ chapter_3/src/db.js
@@ -6,18 +6,19 @@ const db = new DatabaseSync(":memory:");
 
 db.exec(`
     CREATE TABLE user( 
-       id INTEGER,
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
        username TEXT UNIQUE,
        password TEXT,
     )     
 `);
 db.exec(`
     CREATE TABLE todos(
-       id INTEGER,
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
        user_id INTEGER,
-       
        task TEXT,
-       completed INTEGER
-       comple
+       completed BOOLEAN DEFAULT 0,
+       FOREIGN KEY(user_id) REFERENCES user(id),
     )
 `);
+
+export default db;
---
+++ chapter_3/src/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { db } from "../db.js";
+
+const router = express.Routet();
+
+router.post("/register", (req, res) => {});
+
+router.post("/login", (req, res) => {});
+
+export default router;
